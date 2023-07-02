58a8936ac6396015f4ac27d23ded228821b919c2
Boyquotes <webmaster@linux-live-cd.org>
2023-07-02 06:30:02 +0200

    Create doc.py

---
+++ doc.py
@@ -0,0 +1,138 @@
+import bpy
+
+
+def main(context):
+    for ob in context.scene.objects:
+        print(ob)
+
+
+class SimpleOperator(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.simple_operator"
+    bl_label = "Simple Object Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        main(context)
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(SimpleOperator.bl_idname, text=SimpleOperator.bl_label)
+
+
+# Register and add to the "object" menu (required to also use F3 search "Simple Object Operator" for quick access).
+def register():
+    bpy.utils.register_class(SimpleOperator)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.object.simple_operator()
+
+class HelloWorldPanel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Hello World Panel"
+    bl_idname = "OBJECT_PT_hello"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+
+        row = layout.row()
+        row.label(text="Hello world!", icon='WORLD_DATA')
+
+        row = layout.row()
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row.prop(obj, "name")
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add")
+
+
+def register():
+    bpy.utils.register_class(HelloWorldPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(HelloWorldPanel)
+
+
+if __name__ == "__main__":
+    register()
+
+
+print("here")
+
+print(bpy.context.view_layer)
+print(bpy.context.view_layer.objects.active)
+
+list(bpy.data.objects)
+bpy.data.objects['Cube']
+bpy.data.objects[0]
+
+bpy.data.scenes[0].render.resolution_percentage
+bpy.data.scenes[0].objects["Cube"].data.vertices[0].co.x
+
+bpy.data.meshes.new(name="MyMesh")
+
+bpy.context.object["MyOwnProperty"] = 42
+bpy.context.object.get("MyOwnProperty")
+
+bpy.data.scenes["Scene"]["test_prop"] = 34
+bpy.data.scenes["Scene"].get("test_prop", "fallback value")
+
+collection = bpy.data.collections.new("MyTestCollection")
+collection["MySettings"] = {"foo": 10, "bar": "spam", "baz": {}}
+
+del collection["MySettings"]
+
+print(bpy.context.selected_objects)
+print(bpy.context.object)
+print(bpy.context.visible_bones)
+
+#bpy.context.view_layer.objects.active = obj
+
+if bpy.ops.view3d.render_border.poll():
+    bpy.ops.view3d.render_border()
+    
+# modifies the Z axis in place.
+bpy.context.object.location.z += 2.0
+
+# location variable holds a reference to the object too.
+location = bpy.context.object.location
+location *= 2.0
+
+# Copying the value drops the reference so the value can be passed to
+# functions and modified without unwanted side effects.
+location = bpy.context.object.location.copy()
+
+obj = bpy.context.object
+obj.location[2] = 0.0
+obj.keyframe_insert(data_path="location", frame=10.0, index=2)
+obj.location[2] = 1.0
+obj.keyframe_insert(data_path="location", frame=20.0, index=2)
+
+obj = bpy.context.object
+obj.animation_data_create()
+obj.animation_data.action = bpy.data.actions.new(name="MyAction")
+fcu_z = obj.animation_data.action.fcurves.new(data_path="location", index=2)
+fcu_z.keyframe_points.add(2)
+fcu_z.keyframe_points[0].co = 10.0, 0.0
+fcu_z.keyframe_points[1].co = 20.0, 1.0
