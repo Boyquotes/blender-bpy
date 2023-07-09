ae7dce5671d95aa745550029c53921fad4f9c1b3
Boyquotes <webmaster@linux-live-cd.org>
2023-07-09 07:22:33 +0200

    Update text.py

--- text.py
+++ text.py
@@ -1,1 +1,14 @@
  print(bpy.data.texts[1].name)  
+
+name="easyPBI"
+bpy.data.texts[name]
+bpy.data.texts[name].clear()  
+
+name = "metarig.py"
+
+if name in bpy.data.texts:
+    text_block = bpy.data.texts[name]
+    text_block.clear()
+else:
+    text_block = bpy.data.texts.new(name)
+
