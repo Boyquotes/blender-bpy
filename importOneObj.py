9c68fd5dbb3a359840b1038df06ffc2d44edac32
Boyquotes <webmaster@linux-live-cd.org>
2023-07-19 21:54:57 +0200

    Update importOneObj.py FBX

--- importOneObj.py
+++ importOneObj.py
@@ -8,6 +8,14 @@ FILEPATH = '~/blender/bpy/sphere.blend'
 FILEPATHJSON = '~/blender/bpy/save.json'
 DIRECTORY='~/blender/bpy/'
 
+#myModule = bpy.data.texts[0].as_module().importFbx("Cube")
+def importFbx(objName):
+    print(bpy.data.objects)
+    directory = os.path.dirname(bpy.data.filepath)
+    filepath = os.path.join(directory+'/exports', objName + ".fbx")
+    print(filepath)
+    bpy.ops.import_scene.fbx(filepath=filepath)                                                   
+
 def importToScene(FILEPATH):
     with bpy.data.libraries.load(FILEPATH) as (data_from, data_to):
         data_to.objects = [name for name in data_from.objects]
